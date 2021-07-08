d194251e93c537305d97eff968a5584a4c9de12b
viralsir <viralsir2018@gmail.com>
2021-07-08 16:13:14 +0530

    detail page of inwardpurchase is completed.

--- inward_purchase/urls.py
+++ inward_purchase/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns=[
     path("new/",NewInwardBill.as_view(),name="new-purchase"),
     path("closed/",closewindow,name="closed"),
-    path("view/",ViewPurchaseBill.as_view(),name="view-purchase")
+    path("view/",ViewPurchaseBill.as_view(),name="view-purchase"),
+    path("detail/<int:pk>",DetailPurchaseBill.as_view(),name="detail-purchase")
 ]
--- inward_purchase/views.py
+++ inward_purchase/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render
 from prodinward.models import prodinward
 from django import forms
 from .models import inward_purchase
-from django.views.generic import CreateView,ListView
+from django.views.generic import CreateView,ListView,DetailView,DeleteView
 
 
 class NewInwardBill(CreateView):
     model = inward_purchase
     fields = ['date','total_amount','net_amount','gst','discount','due_amount']
     template_name = "inward_purchase/add_bill.html"
-    products=prodinward.objects.filter(billed=0).all()
-    extra_context = {"products":products }
     success_url = '/inward_purchase/view'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = prodinward.objects.filter(billed=0).all()
+        return context
+
 
     def form_valid(self, form):
         print("inside  form valid before save")
@@ -22,12 +26,8 @@ class NewInwardBill(CreateView):
             self.object.products.add(product)
             product.billed=1;
             product.save()
-
-
         self.object.save()
         print("object is saved")
-        # do something with self.object
-        # remember the import: from django.http import HttpResponseRedirect
         return HttpResponseRedirect(self.get_success_url())
 
 
@@ -36,29 +36,34 @@ class ViewPurchaseBill(ListView):
     context_object_name = "bills"
 
 
+class DetailPurchaseBill(DetailView):
+    model = inward_purchase
+
+
+
 # Create your views here.
-class billform(forms.ModelForm):
-
-    class Meta:
-        model=inward_purchase
-        fields='__all__'
-
-class inwardform(forms.ModelForm):
-
-    class Meta:
-        model=prodinward
-        fields='__all__'
-
-def new_bill(request):
-    products=prodinward.objects.filter(is_biiled=False).all()
-    iform=billform()
-    form = inwardform()
-    return render(request,"inward_purchase/add_bill.html",{
-        "products":products,
-        "form":form,
-        "iform":iform
-
-    })
-
+# class billform(forms.ModelForm):
+#
+#     class Meta:
+#         model=inward_purchase
+#         fields='__all__'
+#
+# class inwardform(forms.ModelForm):
+#
+#     class Meta:
+#         model=prodinward
+#         fields='__all__'
+#
+# def new_bill(request):
+#     products=prodinward.objects.filter(is_biiled=False).all()
+#     iform=billform()
+#     form = inwardform()
+#     return render(request,"inward_purchase/add_bill.html",{
+#         "products":products,
+#         "form":form,
+#         "iform":iform
+#
+#     })
+#
 def closewindow(request):
-    return render(request,"inward_purchase/close_window.html")
+     return render(request,"inward_purchase/close_window.html")
